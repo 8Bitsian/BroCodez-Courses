d24805125b3217de9d40d80eb4e0770cec564cc9
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-08-03 15:30:01 -0400

    Use printf statements and use variables with different data types.

--- Python/Notes.md
+++ Python/Notes.md
@@ -1,8 +1,17 @@
-#Setup
-
+# Setup
 There are two things you need to download:
 1. A Python Interpreter
 - Go to [Python.org](https://www.python.org/) and download the latest version
 2. An Integrated Development Environment (IDE)
 - Go to [Jetbrains.com](https://www.jetbrains.com/pycharm/) and download the latest version of the community version (it's free :3)
 
+# Variables
+A variable is a container for a value - like a box. A variable behaves as if it were the value it contains.
+
+There are four different data types @BroCodez will discuss:
+1. Strings: A series of unique characters
+2. Integer: A whole number
+3. Floats: A floating-point number, a number with a decimal portion
+4. Boolean
+
+Each variable should have a unique name
--- Python/main.py
+++ Python/main.py
@@ -1,3 +1,37 @@
 # This is my first Python program
+# Commits are used for ourselves as the compiler won't read this
 print("I like pizza!")
 print("It's really good.")
+
+# Use a variable with strings
+first_name = "Imani Hollie"
+print(first_name)
+print(f"Hello {first_name}!")
+
+food = "pizza"
+print(f"You like {food}.")
+
+email = "IHollie123@website.com"
+print(f"Your email is: {email}")
+
+# Use a variable with integers
+age = 23
+print(f"You are {age} years old.")
+
+itmes = 3
+print(f"You are buying {items} items.")
+
+num_of_students = 30
+print(f"your class has {num_of_students} students.")
+
+# Use a variable with floats
+price = 10.99
+print(f"The price is: ${price}")
+
+gpa = 3.2
+print(f"Your GPA is: {gpa}")
+
+distance = 5.5
+print(f"You ran {distance} kilometers.")
+
+# Use a variable with booleans
