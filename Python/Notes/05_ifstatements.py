b02e7d129022c5861e8b473cd60b39e4684ed027
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-08-03 18:01:20 -0400

    moved comment

--- Python/Notes/05_ifstatements.py
+++ Python/Notes/05_ifstatements.py
@@ -5,16 +5,15 @@ age = int(input("Enter your age: "))
 # We can use if statemnts to check conditions
 if age < 13:
     # Be sure that code underneath the if statement is indented
-    # You don't have to use parenthese, but it makes it easier for readability
     print(f"Becuase you are {age}, you are a child.")
-if else (age < 18):
+if else age < 18:
     # You can check multiple conditions with the if else statement
     print(f"Becuase you are {age}, you are a teenager.")
-elif (age < 25):
+elif age < 25:
     # Another way to write the if else statement is with elif
     print(f"Becuase you are {age}, you are a young adult.")
 elif (age < 65):
-    # Another way to write the if else statement is with elif
+    # You don't have to use parentheses, but it makes it easier for readability
     print(f"Becuase you are {age}, you are an adult.")
 else:
     # The Else statement is a last resort
