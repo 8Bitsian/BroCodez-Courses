1826ee9549c4684db058438be61c53c388321943
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-08-06 16:45:28 -0400

    Update 01_variables.c

--- C/Notes/01_variables.c
+++ C/Notes/01_variables.c
@@ -1,6 +1,18 @@
 #include <studio.h>
 
 int main() {
-  // Variable - A reusable contianer for a value. Behaves as if it were the value it contains.
+  /*
+    Single line comments use two back-slashes (//)
+    Multi-line (block) comments use the backslash and the asterisk (/**/) to enclose the comment over multiple lines
+  
+    Variable - A reusable contianer for a value. Behaves as if it were the value it contains. 
+    There are multiple kinds of variable data types:
+    - Integer (int)
+  */
+
+  // Vairables can be assigned values via the assignment operator (=)
+  // Each statement must be ended w/a semicolon (;)
+  int age = 23;
+  
   return 0;
 }
