9323b8d1fbe069490edac3d488e4fed551c51042
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-08-13 17:47:15 -0400

    Update 01_exercise.py

--- Python/Excercises/01_exercise.py
+++ Python/Excercises/01_exercise.py
@@ -1,5 +1,5 @@
-# Exercise 01: Rectangle Area Calculator
-# 1-3 Notes
+# Exercise 01: Rectangle Area Calculator - Uses Notes 1-3
+# Determine the area of a recetangle by getting and typecasting user input
 
 # Typecast the measurements are floats
 length = float(input("Enter the length: "))
