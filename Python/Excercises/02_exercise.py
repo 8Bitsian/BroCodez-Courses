0e9afd4369bf27a6d341a5877d24d330140f1370
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-08-13 17:49:29 -0400

    Update 02_exercise.py

--- Python/Excercises/02_exercise.py
+++ Python/Excercises/02_exercise.py
@@ -1,5 +1,6 @@
-# Exercise 02: Shopping Cart Program
-# 1-3 Notes
+# Exercise 02: Shopping Cart Program - Uses Notes 1-3
+# Determine the price of items by getting user input and typecasting price and quantity.
+# Ustilize f-strings to output final total to console
 
 # You don't have to typecast str() since it automatically assumes user is inputting a string
 item = input("What item would you like to buy? ")
