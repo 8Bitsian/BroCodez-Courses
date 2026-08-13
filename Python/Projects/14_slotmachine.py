e7490de85d3b17df5542838883cbfdfe0f3c13ee
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-08-13 05:05:33 -0400

    Created PythonNote.md for future reference usage.

---
+++ Python/Projects/14_slotmachine.py
@@ -0,0 +1,29 @@
+# Project 14: Slot Machine Program - Uses Notes 18-28
+# Create a slot machine program using functions and the main method.
+
+# Generate new string for row when spun
+def spin_row():
+    pass
+
+# Print new string to console
+def print_row():
+    pass
+
+# Give user payout if symbols are matching
+def get_payout():
+    pass
+
+# Print main menu options to console
+def menu():
+    pass
+
+# 
+def main():
+    pass
+
+# The program starts by checking if filename is main
+if __name__ == "__main__":
+    # `__` is refered to as "dunder"
+    print(f"Called main.py file is: {__name__}\n")
+    main()
+    print("Exiting main.py file...")
