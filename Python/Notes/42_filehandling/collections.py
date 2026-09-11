0b0628f66e2b04da3950c16e146ecda76af74421
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-09-11 12:08:07 -0400

    Add collections for file handling examples
    Initialized collections for file handling examples including lists and dictionaries.

---
+++ Python/Notes/42_filehandling/collections.py
@@ -0,0 +1,17 @@
+# File Handling (Detection, Writing, Reading)
+# The DREAMHOUSE list is what I will initialize the text.txt file with
+DREAMHOUSE = ["Barbara", "Kenneth", "Nikki", "Raquelle", "Summer", "Ryan"]
+
+# I'll append the original text.txt file w/the remaining collections
+WARHAMMER_40K = ["Imperium of Man", "Eldar", "Tyranids", "Orks", "Necrons", "T'au Empire"]
+
+# `.json` files are made w/key value pairs, like dictionaries are.
+GUNDAM = { "designation": "Gundam ZERO",
+           "model #": "XARX-0",
+           "pilot": "Ray Azumi" }
+
+# Comma separated value `.csv` files are made like excel spreadsheets, like 2D arrays are.
+TRANSFORMERS = [["Designation", "Faction", "Position", "Frame", "Generation"],
+                ["Starscream", "Decepticon", "SIC and Air Commader", "Seeker", 1],
+                ["Sunstreaker", "Autobot", "Frontrunner", "Grounder", 1],
+                ["Bluestreak", "Autobot", "Sniper", "Grounder", 1]]
