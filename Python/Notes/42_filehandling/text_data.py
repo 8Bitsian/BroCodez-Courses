fbc25d1919a262807092e780898c52fefcd8afb7
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-09-11 12:08:40 -0400

    Update text_data.py

--- Python/Notes/42_filehandling/text_data.py
+++ Python/Notes/42_filehandling/text_data.py
@@ -1,1 +1,4 @@
-
+# When writing to files we can use the extensions `.txt`, `.json`, and `.csv`
+TXT_DATA_1 = "I like dolls."
+TXT_DATA_2 = "I also like action figures!"
+TXT_DATA_3 = "Models are also pretty sweet!"
