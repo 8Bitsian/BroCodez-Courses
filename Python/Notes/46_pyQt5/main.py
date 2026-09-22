84616f9016d5af72b31f48f38ebfea5bf5c01fc1
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-09-21 21:34:16 -0400

    modified:   Python/Notes/46_pyQt5/main.py

--- Python/Notes/46_pyQt5/main.py
+++ Python/Notes/46_pyQt5/main.py
@@ -8,7 +8,8 @@ import sys # The `sys` or system module provides access to system-specific param
 # Install Pyqt5 library via the termianl w/the following command: `pip install PyQt5`. Be sure to use the proper capitalization when calling this library.
 # Widgets are the building blocks of any PyQt5 application. They begin with `Q` to help distinguish them from other libraries widgets.
 from PyQt5.QtWidgets import QApplication, QMainWindow 
-# The `QApplication` module
+# The `QApplication` module manages the GUI app's control flow and main settings
+# The `QMainWindow` module is designed for creating the main application window
 
 # Describe what this function does
 def function_name():
