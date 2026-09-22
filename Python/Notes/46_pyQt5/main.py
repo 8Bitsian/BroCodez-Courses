e4fae0b585e819ec1c6aeb8dd6b97e674c6712e5
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-09-21 21:27:27 -0400

    Created 46_PyQt5 folder and main.py and class.py files and updated README.md to include notes on the basics on the PyQt5 graphical user interface (GUI)

---
+++ Python/Notes/46_pyQt5/main.py
@@ -0,0 +1,24 @@
+# pyQt5 GUI
+# A Graphical User Interface (GUI)
+
+# Standard-library imports
+import sys # The `sys` or system module provides access to system-specific parameters and functions
+
+# Third-party or local library imports
+# Install Pyqt5 library via the termianl w/the following command: `pip install PyQt5`. Be sure to use the proper capitalization when calling this library.
+# Widgets are the building blocks of any PyQt5 application. They begin with `Q` to help distinguish them from other libraries widgets.
+from PyQt5.QtWidgets import QApplication, QMainWindow 
+# The `QApplication` module
+
+# Describe what this function does
+def function_name():
+    pass
+
+# Runs the main program
+def main():
+    pass
+
+if __name__ == "__main__":
+    # print(f"Running {__name__}\n")
+    main()
+    # print("Program finished.")
--- Python/README.md
+++ Python/README.md
@@ -639,3 +639,22 @@ To use the threading constructor, import the `threading` module. The `threading`
 The `start()` and `join()` methods control the execution of a Python thread:
 - The `.start()` method begins running the thread's target function in a separate thread so the main program can continue runnign while the task executes
 - The `.join()` method makes the current thread wait to print the next line in the console until another thread finishes
+
+# pyQt5 Graphical User Interface (GUI)
+Install the Pyqt5 library via the termianl w/the following command: `pip install PyQt5`
+
+## Labels
+
+## Images
+
+## Layout Managers
+
+## Buttons
+
+### Radio Buttons
+
+## Checkboxes
+
+## Line Edits
+
+## CSS Styles
